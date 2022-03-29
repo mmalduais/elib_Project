d0c032b0c2a0b387c511a88e78763d1c99f2b5ef
Mohammed Aldoais <mohammedaldoais1996@gmail.com>
2022-03-29 22:52:11 +0300

    first commit for elib_Project

---
+++ public/index.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use coding\app\controllers\AuthorsController;
+use coding\app\controllers\PublishersController;
+use coding\app\system\AppSystem;
+use coding\app\system\Router;
+use coding\app\controllers\UsersController;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));//createImmutable(__DIR__);
+$dotenv->load();
+
+$config=array(
+  'servername'=>$_ENV['DB_SERVER_NAME'],
+  'dbname'=>$_ENV['DB_NAME'],
+  'dbpass'=>$_ENV['DB_PASSWORD'],
+  'username'=>$_ENV['DB_USERNAME']
+
+);
+$system=new AppSystem($config);
+
+Router::get('/users',[UsersController::class,'show']);
+
+
+Router::get('/books',function(){
+  echo "books route path";
+});
+
+Router::get('/new_user',[UsersController::class,'register']);
+Router::get('/new_book',[UsersController::class,'addbook']);
+Router::get('/new_category',[UsersController::class,'addcategory']);
+
+
+Router::get('/remove_user',[UsersController::class,'delete']);
+
+Router::post('/users',[UsersController::class,'show']);
+Router::get('/new_user',[UsersController::class,'newUser']);
+
+Router::post('/save_user',[UsersController::class,'saveUser']);
+Router::get('/save_author',[AuthorsController::class,'createAuthor']);
+$system->start();
+
